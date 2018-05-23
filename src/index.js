2dcd0c0aade81ac0f927650174b2ca9a49df1d24
강재훈 <kangjaehoon@gangjaehun-ui-MacBook-Pro.local>
2018-05-23 12:33:26 +0900

    arranged code

--- src/index.js
+++ src/index.js
@@ -5,13 +5,31 @@
 
 import axios from 'axios';
 
-axios.get('http://localhost:3000/posts').then(res => {
-  res.data.forEach(post => {
+const rootEl = document.querySelector('.root');
 
-    const fragment = document.immportNode(document.querySelector('#post-item').content, true);
-    const pEl = fragment.querySelector('.post-item__title');
+const templates = {
+  postList: document.querySelector('#post-list').content,
+  postItem: document.querySelector('#post-item').content
+}
 
-    pEl.textContent = post.title;
-    document.querySelector('.post-list').appendChild(fragment);
-  })
-})
+
+async function indexPage(){
+
+  const res = await axios.get('http://localhost:3000/posts');
+
+  const listFragment = document.importNode(templates.postList, true);
+    
+    res.data.forEach(post => {
+  
+      const fragment = document.importNode(templates.postItem, true);
+      const pEl = fragment.querySelector('.post-item__title');
+  
+      pEl.textContent = post.title;
+      listFragment.querySelector('.post-list').appendChild(fragment);
+    })
+  
+    rootEl.appendChild(listFragment);
+
+}
+
+indexPage();
