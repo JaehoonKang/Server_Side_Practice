3b8ed338dc99eadcb38d6e5008ed726cdb648f04
강재훈 <kangjaehoon@gangjaehun-ui-MacBook-Pro.local>
2018-05-23 11:53:58 +0900

    First Commit Server Project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-import './index.scss';
 
-document.querySelector('h1').addEventListener('click', e => {
-  alert('Hello World!');
-});
+// document.querySelector('h1').addEventListener('click', e => {
+//   alert('Hello World!');
+// });
+
+import axios from 'axios';
+
+axios.get('http://localhost:3000/posts').then(res => {
+  res.data.forEach(post => {
+
+    const fragment = document.immportNode(document.querySelector('#post-item').content, true);
+    const pEl = fragment.querySelector('.post-item__title');
+
+    pEl.textContent = post.title;
+    document.querySelector('.post-list').appendChild(fragment);
+  })
+})
